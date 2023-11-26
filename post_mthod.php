60ffbe44b6d7126d698bfec8b1c2a57b247bb215
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-11-27 02:30:39 +0900

    Update post_mthod.php

--- post_mthod.php
+++ post_mthod.php
@@ -5,23 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web Input Form</title>
 </head>
-
 <body>
 
-<form method="post" action="process.php">
+
+<form method="post" >
     <label for="N">Enter N:</label>
     <input type="text" name="N" id="N" required>
     <br>
-
-    <label for="A">Enter A (space-separated numbers):</label>
+    <label for="A">Enter A:</label>
     <input type="text" name="A" id="A" required>
     <br>
-
     <input type="submit" value="Submit">
 </form>
+
+    
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $N = $_POST["N"];
+    $A = explode(" ", $_POST["A"]);
+    echo "<p>N: " . htmlspecialchars($N) . "</p>";
+    echo "<p>A: " . implode(", ", array_map('htmlspecialchars', $A)) . "</p>";
+}
+?>
 
 
 </body>
 </html>
+
+
 
 
