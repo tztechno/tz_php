ec8cef40b06145be292ee13fc35f01a9b1e1b8b8
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-11-27 17:00:57 +0900

    Update post_method2.php

--- post_method2.php
+++ post_method2.php
@@ -7,32 +7,34 @@
 </head>
 <body>
 
-
-<form method="post" >
+<form method="post">
     <label for="N">Enter N:</label>
     <input type="text" name="N" id="N" required>
     <br>
     <label for="A">Enter A:</label>
     <input type="text" name="A" id="A" required>
     <br>
-    <label for=“B”>Enter B:</label>
-    <textarea name=“B” id=“B” rows="4" cols="50" required></textarea>
+    <label for="B">Enter B:</label>
+    <textarea name="B" id="B" rows="4" cols="50" required></textarea>
     <br>
+
     <input type="submit" value="Submit">
 </form>
 
-    
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $N = $_POST["N"];
     $A = explode(" ", $_POST["A"]);
-    $B = explode("\n", $_POST[“B”]); // Using "\n" as the delimiter for textarea
+    $B = explode("\n", $_POST["B"]); // Using "\n" as the delimiter for textarea
 
     echo "<p>N: " . htmlspecialchars($N) . "</p>";
     echo "<p>A: " . implode(", ", array_map('htmlspecialchars', $A)) . "</p>";
     echo "<p>B: " . implode(", ", array_map('htmlspecialchars', $B)) . "</p>";
 }
 ?>
+
+</body>
+</html>
 
 
 </body>
